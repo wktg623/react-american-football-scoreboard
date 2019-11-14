9de187742d433d009084f03c0e352087ee12ff32
wktg623 <wktg623@gmail.com>
2019-11-13 22:52:32 -0600

    done with stretch (quarters)

--- src/App.js
+++ src/App.js
@@ -6,30 +6,43 @@ import BottomRow from "./BottomRow";
 function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and 
   //another for the away score.
-const [homeScore, setScore] = useState(0);
-const [awayScore, setScore] = useState(0);
-const homeTeamScores = el => {
-setScore(homeScore + 1);
-}
+  const [homeScore, setHomeScore] = useState(0);
+  const [awayScore, setAwayScore] = useState(0);
+  
 
-const awayTeamScores = el => {
-setScore(awayScore + 1);
-}
+  const homeTeamTouchdown = el => {
+  setHomeScore(homeScore + 7);
+  }
+  
+  const awayTeamTouchdown = el => {
+  setAwayScore(awayScore + 7);
+  }
+
+  const homeTeamFieldGoal = el => {
+    setHomeScore(homeScore + 3);
+    }
+    
+    const awayTeamFieldGoal = el => {
+    setAwayScore(awayScore + 3);
+    }
+  
 
   return (
+
+    
     <div className="container">
       <section className="scoreboard">
         <div className="topRow">
           <div className="home">
-            <h2 className="home__name">Lions</h2>
+            <h2 className="home__name">Vols</h2>
 
             {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
 
             <div className="home__score">{homeScore}</div>
           </div>
-          <div className="timer">{timer}</div>
+          <div className="timer">32 seconds</div>
           <div className="away">
-            <h2 className="away__name">Tigers</h2>
+            <h2 className="away__name">Hogs</h2>
             <div className="away__score">{awayScore}</div>
           </div>
         </div>
@@ -38,13 +51,14 @@ setScore(awayScore + 1);
       <section className="buttons">
         <div className="homeButtons">
           {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
-          <button onClick = {homeTeamScores * 7} className="homeButtons__touchdown">Home Touchdown</button>
-          <button onClick = {homeTeamScores * 3} className="homeButtons__fieldGoal">Home Field Goal</button>
+          <button onClick = {homeTeamTouchdown}  className="homeButtons__touchdown">Home Touchdown</button>
+          <button onClick = {homeTeamFieldGoal}  className="homeButtons__fieldGoal">Home Field Goal</button>
         </div>
         <div className="awayButtons">
-          <button onClick = {awayTeamScores * 7} className="awayButtons__touchdown">Away Touchdown</button>
-          <button onClick = {awayTeamScores * 3} className="awayButtons__fieldGoal">Away Field Goal</button>
+          <button onClick ={awayTeamTouchdown}  className="awayButtons__touchdown">Away Touchdown</button>
+          <button onClick = {awayTeamFieldGoal} className="awayButtons__fieldGoal">Away Field Goal</button>
         </div>
+        
       </section>
     </div>
   );
--- src/BottomRow.js
+++ src/BottomRow.js
@@ -1,26 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
+
 const BottomRow = () => {
+  const [quarterChange, setQuarter] = useState(0);
+
+ 
+
+const nextQuarter = el => {
+
+  setQuarter(quarterChange +1)
+}
+if(quarterChange>4){
+ setQuarter(0)
+ }
+
+
+
+
   return (
     <div className="bottomRow">
       <div className="down">
         <h3 className="down__title">Down</h3>
-        <div className="down__value">3</div>
+        <div className="down__value">1</div>
       </div>
       <div className="toGo">
         <h3 className="toGo__title">To Go</h3>
-        <div className="toGo__value">7</div>
+        <div className="toGo__value">1</div>
       </div>
       <div className="ballOn">
         <h3 className="ballOn__title">Ball on</h3>
-        <div className="ballOn__value">21</div>
+        <div className="ballOn__value">1</div>
       </div>
       <div className="quarter">
         <h3 className="quarter__title">Quarter</h3>
-        <div className="quarter__value">4</div>
+       <div className="quarter__value">{quarterChange}</div>
       </div>
+      
+      <div className = "quarterButton">
+     <button onClick = {nextQuarter}  className="quarterButton_change">Quarter</button>
+   </div>
+   
     </div>
+
   );
 };
 
